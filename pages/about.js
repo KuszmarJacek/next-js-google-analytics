cb4dcdc98c812ac075d19f910ad574ef4c149d5c
Kuszmar Jacek <32854834+vV1235@users.noreply.github.com>
2022-11-27 01:40:04 +0100

    asdf

--- pages/about.js
+++ pages/about.js
@@ -4,22 +4,8 @@ import Script from 'next/script'
 export default function AboutPage() {
     return (
         <div>
-            <Script 
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics-script" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');  
-        `}
-      </Script>
             <Head>
                 <title>About Page</title>
-                
             </Head>
             
             <h1>About Page</h1>
